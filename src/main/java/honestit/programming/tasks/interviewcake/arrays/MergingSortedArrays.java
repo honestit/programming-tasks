da32babfa4ca905f0ed029684a91d5c557f12ce1
honestit <michal.kupisinski@gmail.com>
2020-12-02 09:38:54 +0100

    #6 :: merging null arrays

--- src/main/java/honestit/programming/tasks/interviewcake/arrays/MergingSortedArrays.java
+++ src/main/java/honestit/programming/tasks/interviewcake/arrays/MergingSortedArrays.java
@@ -3,6 +3,9 @@ package honestit.programming.tasks.interviewcake.arrays;
 public class MergingSortedArrays {
 
     public static int[] mergeSortedArrays(int[] first, int[] second) {
+        if (first == null || second == null) {
+            throw new IllegalArgumentException("Arrays must be not null");
+        }
         int[] merged = new int[first.length + second.length];
 
         return merged;
--- src/test/java/honestit/programming/tasks/interviewcake/arrays/MergingSortedArraysTest.java
+++ src/test/java/honestit/programming/tasks/interviewcake/arrays/MergingSortedArraysTest.java
@@ -2,6 +2,11 @@ package honestit.programming.tasks.interviewcake.arrays;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +25,28 @@ class MergingSortedArraysTest {
 
         assertArrayEquals(expected, result);
     }
+
+    @ParameterizedTest
+    @MethodSource("nullArrays")
+    @DisplayName("should throw expection when any array is null")
+    void shouldThrowExceptionWhenAnyArraysIsNull(int[][] arrays) {
+        assertThrows(IllegalArgumentException.class, () -> MergingSortedArrays.mergeSortedArrays(arrays[0], arrays[1]));
+    }
+
+    static Stream<int[][]> nullArrays() {
+        int[][] bothNulls = new int[][] {
+                null, null
+        };
+        int[][] firstNull = new int[][] {
+                null, {}
+        };
+        int[][] secondNull = new int[][] {
+                {}, null
+        };
+        return Stream.of(bothNulls, firstNull, secondNull);
+
+    }
+
+
 
 }
