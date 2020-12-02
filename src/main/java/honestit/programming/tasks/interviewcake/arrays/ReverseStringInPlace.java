5a71841f99f1b6bcd0c34421fd248a689e0bc24d
honestit <michal.kupisinski@gmail.com>
2020-12-02 07:25:21 +0100

    #2 :: reverse empty array

---
+++ src/main/java/honestit/programming/tasks/interviewcake/arrays/ReverseStringInPlace.java
@@ -0,0 +1,11 @@
+package honestit.programming.tasks.interviewcake.arrays;
+
+public class ReverseStringInPlace {
+
+    public static char[] reverse(char[] str) {
+        if (str.length == 0) {
+            return str;
+        }
+        return str;
+    }
+}
---
+++ src/test/java/honestit/programming/tasks/interviewcake/arrays/ReverseStringInPlaceTest.java
@@ -0,0 +1,21 @@
+package honestit.programming.tasks.interviewcake.arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Reverse string in place test cases")
+class ReverseStringInPlaceTest {
+
+    @Test
+    @DisplayName("should reverse empty array")
+    void shouldReverseEmptyArray() {
+        char[] emptyArray = new char[0];
+
+        char[] result = ReverseStringInPlace.reverse(emptyArray);
+
+        assertArrayEquals(emptyArray, result, "Empty array was not properly reversed");
+    }
+
+}
